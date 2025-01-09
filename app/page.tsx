e879c1f9072ea3aca9bd2cd195df8de0a90a9b60
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-09 20:21:32 +0700

    remove accordion from next

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { Accordion, AccordionItem } from './components/Accordion';
 import { RiStarFill } from 'react-icons/ri';
 
 export default function Home() {
