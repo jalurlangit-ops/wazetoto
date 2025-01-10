c85c753e61922b7a35f11a31d553421f73f2ac8a
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:48:32 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,8 @@ function rank() {
           },
 	"author": {
             "@type": "Person",
-            "name": "Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana",
-            "reviewBody": "Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "name": "Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana",
+            "reviewBody": "Wazetoto hadir sebagai platform slot online yang memudahkan pemain dengan menawarkan kemudahan dalam bertransaksi. Salah satu fitur unggulan dari Wazetoto adalah Slot Dana Tanpa Rekening, yang memungkinkan pemain untuk bermain tanpa harus memiliki rekening bank. Dengan menggunakan Dana, pemain dapat langsung melakukan transaksi deposit atau penarikan dengan lebih cepat dan praktis"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921018,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana</title>
         <meta name="description" content="Wazetoto hadir sebagai platform slot online yang memudahkan pemain dengan menawarkan kemudahan dalam bertransaksi. Salah satu fitur unggulan dari Wazetoto adalah Slot Dana Tanpa Rekening, yang memungkinkan pemain untuk bermain tanpa harus memiliki rekening bank. Dengan menggunakan Dana, pemain dapat langsung melakukan transaksi deposit atau penarikan dengan lebih cepat dan praktis" />
         <meta name="keywords" content="Wazetoto, Slot Dana Tanpa Rekening, Deposit Slot Via Dana, Deposit 5000 Via Dana, Cara Deposit Slot Via Ovo, Deposit 5000 Via Qris, Slot Depo 5K Via Dana, Situs Slot Deposit 5000 Via Dana, Deposit Dana 5RB, Game Slot Online Dana" />
@@ -79,8 +94,8 @@ export default function RootLayout({
         <meta name="MobileOptimized" content="width" />
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
-        <meta property="og:title" content="Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana" />
-        <meta property="og:description" content="Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:title" content="Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana" />
+        <meta property="og:description" content="Wazetoto hadir sebagai platform slot online yang memudahkan pemain dengan menawarkan kemudahan dalam bertransaksi. Salah satu fitur unggulan dari Wazetoto adalah Slot Dana Tanpa Rekening, yang memungkinkan pemain untuk bermain tanpa harus memiliki rekening bank. Dengan menggunakan Dana, pemain dapat langsung melakukan transaksi deposit atau penarikan dengan lebih cepat dan praktis" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="wazetoto" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="wazetoto" />
         <meta name="twitter:title" content="Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana" />
-        <meta name="twitter:description" content="Title : Wazetoto : Slot Dana Tanpa Rekening Dan Deposit Slot Via Dana, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="Wazetoto hadir sebagai platform slot online yang memudahkan pemain dengan menawarkan kemudahan dalam bertransaksi. Salah satu fitur unggulan dari Wazetoto adalah Slot Dana Tanpa Rekening, yang memungkinkan pemain untuk bermain tanpa harus memiliki rekening bank. Dengan menggunakan Dana, pemain dapat langsung melakukan transaksi deposit atau penarikan dengan lebih cepat dan praktis" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
